5fd176b86928b7368ef85488610f2f86f6568e38
liz520929 <liz520929@gmail.com>
2026-07-30 11:15:12 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="money-land-v1";
-const FILES=["./","./index.html","./styles.css","./app.js","./manifest.json","./icons/icon-192.svg","./icons/icon-512.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
